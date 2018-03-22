6f719f98907c967ee7d051a9bfe4973aca03cb34
Helen O'Shea <35729078+Osheah@users.noreply.github.com>
2018-03-22 15:46:52 +0000

    added plot

--- pyscripts/desc.py
+++ pyscripts/desc.py
@@ -1,22 +1,50 @@
 #Helen O'Shea 22/03/2018 3
 
 #adapted from https://machinelearningmastery.com/understand-machine-learning-data-descriptive-statistics-python/
+#https://pandas.pydata.org/pandas-docs/stable/visualization.html
+#http://www.scipy-lectures.org/packages/scikit-learn/auto_examples/plot_iris_scatter.html
+
+
 import pandas
 url = "https://raw.githubusercontent.com/Osheah/project/master/data/iris.csv?token=AiEuthVKeloCkMeFSmPeZBQ6oCblwRnRks5avOiwwA%3D%3D"
 names = ['sepalL', 'sepalW', 'petalL', 'petalW', 'name']
-data = pandas.read_csv(url, names=names)
-peek = data.head(10)
+iris = pandas.read_csv(url, names=names)
+peek = iris.head(10)
 print(peek)
-shape = data.shape
+shape = iris.shape
 print(shape)
-types = data.dtypes
+types = iris.dtypes
 print(types)
 pandas.set_option('display.width', 100)
 pandas.set_option('precision', 3)
-description = data.describe()
+description = iris.describe()
 print(description)
-correlations = data.corr(method='pearson')
+correlations = iris.corr(method='pearson')
 print(correlations)
-skew = data.skew()
+skew = iris.skew()
 print(skew)
-import matplotlib
+
+# Load the data
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+from matplotlib import pyplot as plt
+
+# The indices of the features that we are plotting
+x_index = 0
+y_index = 1
+
+# this formatter will label the colorbar with the correct target names
+formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+
+plt.figure(figsize=(5, 4))
+plt.scatter(iris.data[:, x_index], iris.data[:, y_index], c=iris.target)
+plt.colorbar(ticks=[0, 1, 2], format=formatter)
+plt.xlabel(iris.feature_names[x_index])
+plt.ylabel(iris.feature_names[y_index])
+
+plt.tight_layout()
+plt.show()
+
+
+iris.plot(kind="scatter", x="SepalLengthCm", y="SepalWidthCm")
